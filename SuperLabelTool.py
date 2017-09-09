a437c22c2f330d9f3bd8acca5632c64590a9b55d
xpc001 <563853580@qq.com>
2017-09-09 19:21:31 +0800

    first commit

---
+++ SuperLabelTool.py
@@ -0,0 +1,9 @@
+from Form import MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
